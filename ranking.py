8aed0dda0efec06ed2de5a89783a5de9573cff23
samz331 <sziegelbein@verizon.net>
2024-01-22 19:54:50 -0600

    function headers

---
+++ ranking.py
@@ -0,0 +1,10 @@
+class Ranking:
+    def __init__(self, items):
+        self.items = items
+    
+    def getNextPair(self):
+        return None, None
+    
+    def processPair(self, first, second, firstBetter):
+        pass
+
